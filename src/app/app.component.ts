d43ad47debc96601d4883c398648cfbe98b1ecc6
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2019-03-22 23:50:33 +0100

    ConfigCat seems to be working, needs refactoring before moving on

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as configcat from 'configcat-js';
+import {IConfigCatClient} from 'configcat-common/lib/ConfigCatClient';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'flaggo';
+export class AppComponent implements OnInit {
 
-  public navigate(url: string): void {
+  constructor() {
+    this.configCatClient = configcat.createClientWithAutoPoll('a63WCEgeG3MGi8-AI1PXSg/0Cd4SrVP2EC0gTqC_VLclQ', {
+      pollIntervalSeconds: 10
+    });
+  }
+
+  public configCatClient: IConfigCatClient;
+  public showCat: boolean;
+
+  static navigate(url: string): void {
     window.location.href = url;
   }
+
+  ngOnInit(): void {
+    this.configCatClient.getValue('showCatCard', false, value => this.showCat = value);
+  }
+
 }
